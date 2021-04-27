6fa2e990d8001682c729f4613348a6d98f50f020
BartoszPociecha <bary-1992@o2.pl>
2021-04-27 17:58:48 +0200

    Kanał RSS, Najczęściej komentowane posty, sitemap, Markdow

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
+sitemaps = {
+    'posts': PostSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^blog/', include(('blog.urls', 'app_name'),
                            namespace='blog')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
 app_name= 'blog'
